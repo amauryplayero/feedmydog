07a3898c4a774e6131c0b360d745fa94a7cefe0d
Amaury <amaury.lopezdiaz@gmail.com>
2023-10-14 22:29:24 -0400

    removed empty folders

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -15,8 +15,8 @@ console.log(editingComment)
     <footer className="flex justify-center items-center bg-transparent text-black h-16">
       <p>follow amaury on <a target="blank" href="https://www.linkedin.com/in/amaury-lopez-4048671a8/">linkedin</a></p>
       <div className="absolute bottom-0 right-4 overflow-hidden">
-       <Image className={`transition-all w-28 md:w-72 fixed -bottom-14 md:-bottom-${editingComment ? '44' : '36'} left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image>
-
+       {/* <Image className={`transition-all w-28 md:w-72 fixed -bottom-14 md:-bottom-${editingComment ? '36' : '36'} left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image> */}
+       <Image className={`transition-all w-28 md:w-72 fixed -bottom-14 md:-bottom-36 left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image>
       </div>
     </footer>
   )
--- src/app/Components/NavBar.tsx
+++ src/app/Components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar:React.FC = () => {
     <div className="w-full flex justify-between h-[60px] bg-gradient-to-r to-[#c66ec6] from-pink-400 text-black px-8">
         <div className="flex items-center overflow-hidden">
           <div className='absolute -left-20 h-[60px] overflow-y-hidden'>
-              <Image className="w-[350px] relative rotate-90 -top-32 -left-12" src={kibbles} alt="patty melt" height={40} width={200}></Image>
+              <Image priority className="w-[350px] relative rotate-90 -top-32 -left-12" src={kibbles} alt="patty melt" height={40} width={200}></Image>
           </div>
 
           <p className="text-3xl font-bold text-white ml-20">Feed my dog</p>
