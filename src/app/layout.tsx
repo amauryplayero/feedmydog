2fc8163f32bd4e1c5cbe77a080193b419c4ae702
Amaury <amaury.lopezdiaz@gmail.com>
2023-10-16 20:14:40 -0400

    added GA4 script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './Components/NavBar'
+import Script from 'next/script'
+const GTM_ID = 'GTM-5DMFF6DH';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +23,15 @@ export default function RootLayout({
     {/* <NavBar /> */}
      
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
     </>
