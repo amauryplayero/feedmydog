d4199fb76c05a2ccd084e0d1320af925cbeef607
Amaury <amaury.lopezdiaz@gmail.com>
2023-10-16 23:16:23 -0400

    added improvement to pattie on the footer

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -16,7 +16,7 @@ console.log(editingComment)
       <p>follow amaury on <a target="blank" href="https://www.linkedin.com/in/amaury-lopez-4048671a8/">linkedin</a></p>
       <div className="absolute bottom-0 right-4 overflow-hidden">
        {/* <Image className={`transition-all w-28 md:w-72 fixed -bottom-14 md:-bottom-${editingComment ? '36' : '36'} left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image> */}
-       <Image className={`transition-all w-28 md:w-72 fixed -bottom-14 md:-bottom-36 left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image>
+       <Image className={`transition-all w-28 md:w-72 fixed ${editingComment ? '-bottom-6' : '-bottom-14'} md:-bottom-${editingComment ? '36' : '34'} left-0 pointer-events-none`} src={pattieFooter} alt="patty melt" height={40} width={200}></Image>
       </div>
     </footer>
   )
